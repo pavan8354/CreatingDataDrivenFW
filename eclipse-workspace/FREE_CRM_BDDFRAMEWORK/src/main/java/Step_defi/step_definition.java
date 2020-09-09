b514f8e73040def8d7a33ed4d5b65f783b66fe6e
username <pavansdmitmech@gmail.com>
2020-09-09 16:44:18 +0530

    NEW BDD FRAMEWORK UPDATED

---
+++ eclipse-workspace/FREE_CRM_BDDFRAMEWORK/src/main/java/Step_defi/step_definition.java
@@ -0,0 +1,34 @@
+package Step_defi;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class step_definition {
+	
+	@Given("^user is already on Login Page$")
+	public void user_is_already_on_Login_Page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^title of login page is Free CRM$")
+	public void title_of_login_page_is_Free_CRM() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user enters \"([^\"]*)\" and \"([^\"]*)\"$")
+	public void user_enters_and(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user clicks on login button$")
+	public void user_clicks_on_login_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
